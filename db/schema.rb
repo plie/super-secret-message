55be5e42172de303f74b785480b0d77f6436e3d0
Jenee Geer <jenee@plie.codes>
2018-09-16 08:05:32 -0500

    add users to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180809201430) do
+ActiveRecord::Schema.define(version: 20180916125935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20180809201430) do
     t.string   "token",         null: false
     t.string   "password_hash", null: false
     t.text     "message_body"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "cell",          null: false
+    t.string   "password_hash", null: false
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
