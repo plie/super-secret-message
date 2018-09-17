7c9904868e44b4fc924effde8b6230a02f7a6c9b
Jenee Geer <jenee@plie.codes>
2018-09-17 15:27:28 -0500

    beginning to add Users and Sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+	def show
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def set_user
+	end
+
+
+end
