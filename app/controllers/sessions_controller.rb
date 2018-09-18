35637d7805b13fa1b8d683ea0ad20a85ba58c37f
Jenee Geer <jenee@plie.codes>
2018-09-18 12:56:01 -0500

    work in progress. building out user authentication piece

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,29 @@
 class SessionsController < ApplicationController
-	before_action :authenticate
+	skip_before_action :authenticate, only: [:new, :create, :destroy]
+
+	def new
+		render layout: 'login'
+	end
 
 	def create
-		@user = User.find(:user_id)
-		if @user && @user.password == params(:password_hash)
+		@user = User.find_by_handle(params[:handle])
+		if @user && @user.password == params(:password)
 			@session[:session_id] = @user.id
 		else
-			flash[:warning] = "Unauthorized. Please enter correct password"
+			flash[:warning] = "Unauthorized. Please enter correct username and password"
 			redirect_to '/'
 		end
 	end
 
 	def destroy
 		@session[:session_id] = nil
-		redirect_to '/'
+		redirect_to new_session_path
 	end
 
 	private
 
-	def set_user
-		@current_user = @user
+	def user_params
+		params.require(:user).permit(:handle, :cell, :password)
 	end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	skip_before_action :authenticate, only: [:new, :create]
 	before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 	def show
@@ -10,22 +11,33 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)
+		@user.password_hash = user_params[:password_hash]
 		if @user.save
 			flash[:success] = "New account created. Welcome!"
-			redirect_to "/"  # change to user account page
+			redirect_to user_path
 		else
 			flash[:danger] = "New account not created. Please try again"
-			redirect_to "/"
+			redirect_to new_user_path
 	end
 
 	def edit
 	end
 
 	def update
+		@user.assign_attributes(user_params)
+		if @user.save
+			flash[:success] = "Your changes were saved."
+			redirect_to user_path
+		else
+			flash[:danger] = "Your new information was not saved. Please try again."
+			redirect_to edit_user_path
+		end
 	end
 
 	def destroy
 		@user.destroy
+		flash[:success] = "Your account has been destroyed. Thank you for using Super Secret Message!"
+		redirect_to '/'
 	end
 
 	private
@@ -35,7 +47,7 @@ class UsersController < ApplicationController
 	end
 
 	def user_params
-		params.require(:cell).permit(:password_hash)
+		params.require(:user).permit(:cell, :password_hash, :handle)
 	end
 
 end
